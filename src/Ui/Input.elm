b5f77e149463e780e59db558299b7945a08f7fc3
Mathmoose <materbyl@gmail.com>
2023-04-11 10:27:23 +0200

    Input module (#19)

--- src/Ui/Input.elm
+++ src/Ui/Input.elm
@@ -1,12 +1,19 @@
 module Ui.Input exposing (search)
 
+{-| an input is an element used to collect user data within a web form.
+It allows users to enter information such as text, numbers, or other data types, which can then be submitted to a server for processing.
+
+@docs search
+
+-}
+
 import Accessibility.Styled as Styled
+import Accessibility.Styled.Aria as Aria
 import Css
 import Extra.Styled as Styled
 import Html.Styled exposing (Attribute)
-import Html.Styled.Attributes exposing (css, property)
+import Html.Styled.Attributes exposing (css)
 import Html.Styled.Events
-import Json.Encode as JE
 import Rpx exposing (rpx)
 import Ui.Button
 import Ui.Color exposing (toCssColor)
@@ -24,17 +31,27 @@ inputTextBaseStyle =
         ]
 
 
+{-|
+
+    Renders a search input field.
+
+    Takes the following configuration:
+    - `value`:      The value of the search field
+    - `onInput`:    A message that takes the new value of the input
+    - `onClear`:    The message that should be emitted when a user presses the clear button
+    - `onSearch:    Maybe a message that should be emitted when the user presses the search button
+
+-}
 search :
     List (Styled.Attribute Never)
     ->
-        { searchLabel : String
-        , value : String
+        { value : String
         , onInput : String -> msg
         , onClear : msg
         , onSearch : Maybe msg
         }
     -> Styled.Html msg
-search attrs { searchLabel, value, onInput, onClear, onSearch } =
+search attrs { value, onInput, onClear, onSearch } =
     Styled.div ([ inputTextBaseStyle, css [ Css.borderRadius (rpx 20) ] ] ++ attrs) <|
         [ Ui.Button.iconButton [ css [ Css.cursor Css.default ] ]
             { tooltip = "search"
@@ -43,7 +60,7 @@ search attrs { searchLabel, value, onInput, onClear, onSearch } =
             }
         , Styled.inputText value
             [ Html.Styled.Events.onInput onInput
-            , property "autocomplete" (JE.string searchLabel)
+            , Aria.label "search-text-input-field"
             , css [ Css.border (Css.px 0), Css.flexGrow (Css.num 1), Css.marginRight (Css.px 7), Css.outline Css.none ]
             ]
         , Styled.when (value /= "") <|
