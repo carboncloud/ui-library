e0f31562e570443873f00571bd6a76bbaddbec61
Mathmoose <materbyl@gmail.com>
2023-10-04 09:11:37 +0200

    Update icons module (#32)

--- app/src/Stories/Icon.elm
+++ app/src/Stories/Icon.elm
@@ -1,6 +1,6 @@
 module Stories.Icon exposing (..)
 
-import Css exposing (displayFlex, flex)
+import Css exposing (displayFlex)
 import Html exposing (Html)
 import Html.Styled as Styled
 import Html.Styled.Attributes exposing (css)
@@ -8,10 +8,9 @@ import Storybook.Component exposing (Component)
 import Storybook.Controls
 import Svg.Styled exposing (toUnstyled)
 import Ui.Button exposing (ButtonEmphasis(..))
-import Ui.Color exposing (toCssColor)
-import Ui.Icon as Icon
-import Ui.Palette
-import Ui.Shadow
+import Ui.Icon as Icon exposing (Icon)
+import Ui.Text as Text
+import Ui.TextStyle as TextStyle
 
 
 main : Component () Msg
@@ -35,7 +34,18 @@ type Msg
 view : Html Msg
 view =
     toUnstyled <|
-        Styled.div [ css [ displayFlex, Css.property "gap" "20px" ] ] <|
-            ([ Icon.edit, Icon.settings, Icon.comment, Icon.search, Icon.close, Icon.chevronUp, Icon.chevronRight, Icon.chevronDown, Icon.chevronLeft, Icon.chevronRight, Icon.newWindow ]
-                |> List.map (Styled.div [ css [ Css.border3 (Css.px 1) Css.solid (toCssColor Ui.Palette.gray900), Css.backgroundColor <| toCssColor Ui.Palette.gray050, Css.width (Css.px 32), Css.height (Css.px 32) ] ] << List.singleton << Icon.view)
+        Styled.div [ css [ displayFlex, Css.flexDirection Css.column, Css.property "gap" "45px" ] ]
+            [ iconGroup "Actions" [ Icon.edit, Icon.copy, Icon.settings, Icon.delete, Icon.comment, Icon.search, Icon.close, Icon.newWindow, Icon.more ]
+            , iconGroup "Chevron" [ Icon.chevronUp, Icon.chevronRight, Icon.chevronDown, Icon.chevronLeft ]
+            ]
+
+
+iconGroup : String -> List Icon -> Styled.Html msg
+iconGroup name icons =
+    Styled.div []
+        [ Text.view TextStyle.heading3 name
+        , Styled.div [ css [ displayFlex, Css.property "gap" "20px", Css.marginTop (Css.px 10) ] ] <|
+            (icons
+                |> List.map (Styled.div [ css [ Css.width (Css.px 24), Css.height (Css.px 24) ] ] << List.singleton << Icon.view24x24)
             )
+        ]
